3c59af30d1730e8df7fa79c2b2567a405f3b6f92
KurbinaDaria <d.kurbina@ukma.edu.ua>
2024-10-08 14:26:40 +0300

    Видалено папки bookmar і bookmarks

--- bookmark/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import BookmarkList, BookmarkDetail, BookmarkFavorite
-
-urlpatterns = [
-    path('', BookmarkList.as_view(), name='bookmark-list'),
-    path('<int:pk>/', BookmarkDetail.as_view(), name='bookmark-detail'),
-    path('<int:pk>/favorite/', BookmarkFavorite.as_view(), name='toggle-favorite'),
-]
